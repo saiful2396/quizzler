ed42aed28702f7124f586c7e8f75926db9c5b487
Saiful Islam <saifulislampp23@gmail.com>
2020-08-26 23:45:09 +0600

    to do update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(MyApp());
 
@@ -7,7 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: QuizScreen(),
+      home: Scaffold(
+          backgroundColor: Colors.grey.shade900,
+          body: SafeArea(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: QuizScreen(),
+          ))),
     );
   }
 }
@@ -18,42 +28,108 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
+
+  List<Icon> scoreKeeper = [];
+
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain.getCorrectAnswer();
+    setState(() {
+      if(quizBrain.isFinished() == true){
+        Alert(
+          context: context,
+          title: "Finished!",
+          desc: "You've reached the end of the quiz.",
+          buttons: [
+            DialogButton(
+              child: Text(
+                "cancel",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => Navigator.pop(context),
+              width: 120,
+            )
+          ],
+        ).show();
+        quizBrain.reset();
+        scoreKeeper = [];
+      } else if (userPickedAnswer == correctAnswer) {
+        scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+      } else {
+        scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+      }
+      quizBrain.nextQuestion();
+    });
+  }
+
+  void isFinished(context){
+    Alert(
+      context: context,
+      title: "Finished!",
+      desc: "You've reached the end of the quiz.",
+      buttons: [
+        DialogButton(
+          child: Text(
+            "cancel",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () => Navigator.pop(context),
+          width: 120,
+        )
+      ],
+    ).show();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black26,
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              flex: 10,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Center(
-                  child: Text('This the question place that we are place here?',
-                  style: TextStyle(color: Colors.white, fontSize: 20),),
-                ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                // for update questions from List
+                quizBrain.getQuestionText(),
+                style: TextStyle(color: Colors.white, fontSize: 20),
               ),
             ),
-            Spacer(),
-            Container(
-              color: Colors.green,
-              child: FlatButton(onPressed:()=>null,
-                  child: Text('True',style: TextStyle(color: Colors.white),)),
-            ),
-            SizedBox(height: 20,),
-            Container(
-              color: Colors.red,
-              child: FlatButton(
-                  onPressed: ()=> null,
-                  child: Text('False',style: TextStyle(color: Colors.white),)),
-            ),
-            SizedBox(height: 50,)
-          ],
+          ),
         ),
-      ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  checkAnswer(true);
+                },
+                child: Text(
+                  'True',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                )),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  checkAnswer(false);
+                },
+                child: Text(
+                  'False',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                )),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        ),
+      ],
     );
   }
 }
---
+++ lib/quiz_brain.dart
@@ -0,0 +1,46 @@
+import 'package:quizzler/question.dart';
+
+class QuizBrain {
+  int _questionNumber = 0;
+  List<Questions> _questionBank = [
+    Questions('You can lead a cow down stairs but not up  stairs.', false),
+    Questions('Approximately on quarter of human bones are in the feet.', true),
+    Questions('A slug\'s blood is green.', true),
+    Questions('Some cats are actually allergic to human', true),
+    Questions('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".',true),
+    Questions('It is illegal to pee in the Ocean in Portugal.',true),
+    Questions('No piece of square dry page can be folded in half more than 7 times.',false),
+    Questions('In London, UK, if you happen to die in the house of Parliament, you are technically'
+        'entitled to a state funeral, because the building is considered too sacred a place.',true),
+    Questions('The loudest sound produced by any animal is 188 decibels. that animal is the African Elephant',false),
+    Questions('The total surface area of two human lungs is approximately 70 square meter',true),
+    Questions('Google was originally called\"Backrub\".',true),
+    Questions('Chocolate affects a dog\'s heart and nerves system; a few ounce are enough to kill a small dog.',true),
+    Questions('In west Verginia USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',true),
+  ];
+
+  void nextQuestion(){
+    if(_questionNumber < _questionBank.length - 1){
+      _questionNumber++;
+    }
+  }
+
+  String getQuestionText(){
+    return _questionBank[_questionNumber].questionText;
+  }
+  bool getCorrectAnswer(){
+    return _questionBank[_questionNumber].questionAnswer;
+  }
+
+  bool isFinished(){
+    if(_questionNumber >= _questionBank.length - 1){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  void reset(){
+    _questionNumber = 0;
+  }
+}
